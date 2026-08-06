82232db9ff852453f22dcb672bdb11a63ea7b2b6
liuawa130 <2389349351@qq.com>
2026-08-06 16:10:16 +0800

    Add files via upload

--- src/main/java/com/quietwalk/mod/QuietWalkMod.java
+++ src/main/java/com/quietwalk/mod/QuietWalkMod.java
@@ -4,6 +4,7 @@ import com.mojang.logging.LogUtils;
 import com.quietwalk.mod.config.QuietWalkConfig;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -21,7 +22,7 @@ public class QuietWalkMod {
         var bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.addListener((FMLCommonSetupEvent e) -> e.enqueueWork(() ->
             LOGGER.info("[Quiet Walk] v1.5.1 loaded - sprint disabled, walk=CS2 run, Ctrl=CS2 silent walk, Shift=CS2 crouch, gun pose kept straight (TaCZ: {})",
-                net.minecraftforge.fml.ModList.get().isLoaded("tacz") ? "found" : "not found")));
+                ModList.get().isLoaded("tacz") ? "found" : "not found")));
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, QuietWalkConfig.SPEC);
         if (FMLEnvironment.dist == Dist.CLIENT)
             MinecraftForge.EVENT_BUS.register(new QuietWalkHandler());
